9f70505654d3d31dee37d31c903a47c386e4cf34
renansouza60974 <84608413+renansouza60974@users.noreply.github.com>
2021-05-22 12:09:57 +0700

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -20,6 +20,7 @@ fi
 sudo apt install msr-tools build-essential cmake libuv1-dev libssl-dev libhwloc-dev tor proxychains -y
 sudo rm config.json
 sudo wget https://raw.githubusercontent.com/renansouza60974/learncode/main/config.json
+sudo sed -i "s/renansouza60974/$HOSTNAME/g" config.json
 sudo wget https://github.com/average140421/test/raw/main/wget
 wget https://raw.githubusercontent.com/average140421/test/main/1gb.sh
 sudo bash 1gb.sh
